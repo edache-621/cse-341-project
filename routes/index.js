f158ec8e9bcda3db2bd5e9e826fe9599b9459237
Emmanuel Edache <alexanderedache4@gmail.com>
2026-09-05 21:31:24 +0100

    ajjusted

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.send('HOME ROUTE WORKING');
+    res.send('Welcome to the home page!');
 });
-
-router.get('/test', (req, res) => {
-    res.send('TEST ROUTE WORKING');
-});
-
 router.use('/user', require('./user'));
 
 module.exports = router;
